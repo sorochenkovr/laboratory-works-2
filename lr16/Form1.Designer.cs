e9282b9f05e5f766ece904762093b9f9c600cf98
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2021-04-01 02:50:09 +0300

    lr16

---
+++ lr16/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace lr16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(417, 178);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(435, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(277, 178);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 267);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Загрузить данные";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 333);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ lr16/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lr16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = Application.StartupPath;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ShowData(openFileDialog1.FileName);
+            }
+        }
+
+        DataStorage data;
+        private void ShowData(String datapath)
+        {
+            try
+            {
+                data = DataStorage.DataCreator(datapath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something's wrong");
+            }
+            dataGridView1.DataSource = data.GetRawData();
+            dataGridView1.ReadOnly = true;
+            dataGridView2.DataSource = data.GetSummaryData();
+            dataGridView2.ReadOnly = true;
+        }
+        class RawDataItem
+        {
+            public String Name { get; set; }
+            public String Group { get; set; }
+            public float Price1 { get; set; }
+            public float Price2 { get; set; }
+            public float Total { get { return Price2; } }
+            public float Max { get; set; }
+        }
+        class SummaryDataItem
+        {
+            public String GroupName { get; set; }
+            public float GroupSum { get; set; }
+            public float GroupMax { get; set; }
+        }
+        class Utils
+        {
+            private static Dictionary<String, String> dict;
+            static Utils()
+            {
+                if (dict == null)
+                {
+                    dict = new Dictionary<string, string>();
+                    dict.Add("Продукты", "Продукты питания");
+                    dict.Add("Химия", "Бытовая химия");
+                    dict.Add("Канцелярия", "Канцелярские принадлежности");
+                    dict.Add("Напитки", "Прохладительные напитки");
+                }
+            }
+            public static String GetGroupByName(String stroka)
+            {
+                if (dict.ContainsKey(stroka))
+                    return dict[stroka];
+                else
+                    return "???";
+            }
+        }
+        interface DataInterface
+        {
+            List<RawDataItem> GetRawData();
+            List<SummaryDataItem> GetSummaryData();
+        }
+        class DataStorage : DataInterface
+        {
+            public bool IsReady
+            {
+                get
+                {
+                    if (rawdata == null) return false;
+                    else return true;
+                }
+            }
+            private List<RawDataItem> rawdata;
+            private List<SummaryDataItem> sumdata;
+            private char dev = '%';
+            private DataStorage() { }
+            private void BuildSummary()
+            {
+                Dictionary<String, float> tmp = new Dictionary<String, float>();
+                foreach (var item in rawdata)
+                {
+                    if (tmp.ContainsKey(item.Group))
+                        tmp[item.Group] += item.Total;
+                    else
+                        tmp[item.Group] = item.Total;
+                }
+                sumdata = new List<SummaryDataItem>();
+                foreach (var item in tmp)
+                {
+                    sumdata.Add(new SummaryDataItem()
+                    {
+                        GroupName = Utils.GetGroupByName(item.Key),
+                        GroupSum = item.Value,
+                    });
+                }
+            }
+            private void GetMax()
+            {
+                Dictionary<String, float> tmp1 = new Dictionary<String, float>();
+                foreach (var item1 in rawdata)
+                {
+                    if (tmp1.ContainsKey(item1.Group))
+                        item1.Max = Math.Max(item1.Price2, item1.Price2);
+                }
+                sumdata = new List<SummaryDataItem>();
+                foreach (var item1 in tmp1)
+                {
+                    sumdata.Add(new SummaryDataItem()
+                    {
+                        GroupMax = item1.Value,
+                    });
+                }
+            }
+            private bool InitData(String datapath)
+            {
+                rawdata = new List<RawDataItem>();
+                try
+                {
+                    StreamReader sr = new StreamReader(datapath, Encoding.UTF8);
+                    String line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] items = line.Split(dev);
+                        var item = new RawDataItem()
+                        {
+                            Name = items[0].Trim(),
+                            Group = items[1].Trim(),
+                            Price1 = Convert.ToSingle(items[2].Trim()),
+                            Price2 = Convert.ToSingle(items[3].Trim())
+                        };
+                        rawdata.Add(item);
+                    }
+                    sr.Close();
+                    BuildSummary();
+                }
+                catch (IOException ex)
+                {
+                    return false;
+                }
+                return true;
+            }
+            public static DataStorage DataCreator(String path)
+            {
+                DataStorage d = new DataStorage();
+                if (d.InitData(path))
+                    return d;
+                else
+                    return null;
+            }
+
+            public List<RawDataItem> GetRawData()
+            {
+                if (this.IsReady)
+                    return rawdata;
+                else
+                    return null;
+            }
+
+            public List<SummaryDataItem> GetSummaryData()
+            {
+                if (this.IsReady)
+                    return sumdata;
+                else
+                    return null;
+            }
+        }
+    }
+}
