ce1a357116ea72d4cef38df635c6f99751d04c71
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2021-05-08 23:19:06 +0300

    lr17

---
+++ lr_17/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace lr_17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(340, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "y";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(563, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(340, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(537, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(340, 210);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(219, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(171, 265);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 17);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(387, 352);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 17);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(387, 402);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(16, 17);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 473);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ lr_17/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+
+namespace lr_17
+{
+    public partial class Form1 : Form
+    {
+        Region regResult = new Region();
+        Region regResult2 = new Region();
+        Brush active = Brushes.Red;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var gr = e.Graphics;
+            Rectangle rect = new Rectangle(280, 70, 120, 130);
+            Rectangle circle = new Rectangle(150, 70, 250, 250);
+            Rectangle circle2 = new Rectangle(183, 100, 185, 185);
+            Rectangle circle3 = new Rectangle(160, 200, 120, 130);
+
+            Pen pen = new Pen((Color.Black),3);
+            pen.EndCap = LineCap.ArrowAnchor;
+            e.Graphics.DrawLine(pen, 120, 200, 430, 200);
+            Pen pen2 = new Pen((Color.Black), 3);
+            pen2.EndCap = LineCap.ArrowAnchor;
+            e.Graphics.DrawLine(pen2, 280, 340, 280, 40);
+            Pen pen3 = new Pen((Color.Black), 3);
+            e.Graphics.DrawLine(pen3, 170, 310, 420, 50);
+
+            GraphicsPath gpRect = new GraphicsPath();
+            gpRect.AddRectangle(rect);
+            GraphicsPath gpEllip = new GraphicsPath();
+            gpEllip.AddEllipse(circle);
+            GraphicsPath gpEllip2 = new GraphicsPath();
+            gpEllip2.AddEllipse(circle2);
+            GraphicsPath gpEllip3 = new GraphicsPath();
+            gpEllip3.AddEllipse(circle3);
+            GraphicsPath gpSector = new GraphicsPath();
+            gpSector.AddPie(circle, -270, 42);
+
+            Region regRect = new Region(gpRect);
+            Region regEllip = new Region(gpEllip);
+            
+            regResult = new Region(gpRect);
+            regResult.Intersect(regEllip);
+            regResult.Exclude(gpEllip2);
+            regResult2 = new Region(gpSector);
+            regResult2.Intersect(gpEllip3);
+            regResult2.Exclude(gpEllip2);
+
+            gr.DrawRectangle(Pens.Red, rect);
+            gr.DrawEllipse(Pens.Green, circle);
+            gr.DrawEllipse(Pens.Green, circle2);
+            gr.DrawEllipse(Pens.Red, circle3);
+
+            gr.FillRegion(active, regResult);
+            gr.FillRegion(active, regResult2);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int X = e.X, Y = e.Y;
+            Brush one = active;
+            if (regResult.IsVisible(X, Y))
+            {
+                active = Brushes.Red;
+            }
+            else active = Brushes.Green;
+            Invalidate();
+
+            Text = string.Format("Коорд: {0},{1}", e.X, e.Y);
+            
+        }
+    }
+}
