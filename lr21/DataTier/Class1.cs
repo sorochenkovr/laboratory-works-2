c7f00280cf2413b7782cf1b584a224be15c1ba56
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2021-05-20 22:19:36 +0300

    lr21

---
+++ lr21/DataTier/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DataTier
+{
+    public class Students
+    {
+        public string ФИО { get; set; }
+        public string Группа { get; set; }
+        public float Курс { get; set; }
+        public float Задолженности { get; set; }
+        public static Students Create(string str)
+        {
+            Students info = new Students();
+            string[] elem = str.Split('*');
+            info.ФИО = elem[0].Trim();
+            info.Группа = elem[1].Trim();
+            info.Курс = Convert.ToSingle(elem[2]);
+            elem[2] = elem[2].Replace('.', ',');
+            info.Задолженности = Convert.ToSingle(elem[3]);
+            elem[3] = elem[3].Replace('.', ',');
+            return info;
+        }
+    }
+
+    public class StudInfo
+    {
+
+        public static List<Students> StIn(StreamReader txt)
+        {
+            List<Students> list = new List<Students>();
+            string line;
+            while ((line = txt.ReadLine()) != null)
+            {
+                list.Add(Students.Create(line));
+            }
+            return list;
+        }
+    }
+}
---
+++ lr21/LogicTier/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using DataTier;
+
+namespace LogicTier
+{
+    public class StudentValues
+    {
+        private Students st;
+        public StudentValues(Students s)
+        {
+            st = s;
+        }
+        public string FIO
+        {
+            get { return st.ФИО; }
+            set { st.ФИО = value; }
+        }
+        public string Group
+        {
+            get { return st.Группа; }
+            set { st.Группа = value; }
+        }
+        public float Curs
+        {
+            get { return st.Курс; }
+            set { st.Курс = value; }
+        }
+        public float Debt
+        {
+            get { return st.Задолженности; }
+            set { st.Задолженности = value; }
+        }
+
+
+        public String AllInfo
+        {
+            get { return st.ФИО + " : " + st.Группа + " : " + st.Курс.ToString() + " : " + st.Задолженности.ToString(); }
+        }
+    }
+    public class Summary
+    {
+        StreamReader txt = new StreamReader(@"input.txt");
+        private List<StudentValues> st = new List<StudentValues>();
+
+        float sum1, sum2 = 0;
+        public Summary()
+        {
+
+            List<Students> tmp = StudInfo.StIn(txt);
+            foreach (var v in tmp)
+            {
+                st.Add(new StudentValues(v));
+                if (v.Задолженности == 0)
+                {
+                    sum1 += 1;
+                }
+
+            }
+
+            sum2 = (from v in tmp
+                  where v.Задолженности > 0
+                  select v.Задолженности).Sum();
+
+        }
+        public float СуммарноеКоличествоЗадолжностей
+        {
+            get { return sum2; }
+        }
+        public float СуммарноеКоличествоЛюдей
+        {
+            get { return sum1; }
+        }
+    }
+}
