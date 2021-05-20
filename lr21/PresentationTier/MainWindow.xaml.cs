b67331b7f989ada219e256529670eb7ede132987
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2021-05-20 22:21:11 +0300

    Add files via upload

---
+++ lr21/PresentationTier/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using LogicTier;
+
+namespace PresentationTier
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Summary logicTire = new Summary();
+            this.DataContext = logicTire;
+            InitializeComponent();
+        }
+    }
+}
